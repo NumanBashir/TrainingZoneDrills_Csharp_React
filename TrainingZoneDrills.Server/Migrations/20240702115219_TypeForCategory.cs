9e55779363839c481086d90da93485f6b8a0ce44
NumanBashir <sportybashir@gmail.com>
2024-07-02 16:27:52 +0200

    orm update

---
+++ TrainingZoneDrills.Server/Migrations/20240702115219_TypeForCategory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrainingZoneDrills.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class TypeForCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Categories");
+        }
+    }
+}
