7b48efbffb9d466c822990dc9ddc23a9f4f2cc6f
NumanBashir <sportybashir@gmail.com>
2024-07-02 13:47:06 +0200

    Connect database to project

---
+++ TrainingZoneDrills.Server/Context/TrainingZoneContext.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TrainingZoneDrills.Server.Models;
+
+namespace TrainingZoneDrills.Server.Context
+{
+    public class TrainingZoneContext : DbContext
+    {
+        public DbSet<Drill> Drills { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Equipment> Equipments { get; set; }
+        public DbSet<DrillCategory> DrillCategories { get; set; }
+        public DbSet<DrillEquipment> DrillEquipments { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=DrillBank;Trusted_Connection=True;");
+        }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Below we are defining the many-to-many relationships between the Drill, Category, and Equipment models.
+            modelBuilder.Entity<DrillCategory>()
+                .HasKey(dc => new { dc.DrillId, dc.CategoryId });
+
+            modelBuilder.Entity<DrillEquipment>()
+                .HasKey(de => new { de.DrillId, de.EquipmentId });
+
+            // Below we are defining the relationship between the Drill and DrillCategory models.
+            // We are saying that a Drill can have many DrillCategories.
+            // We are also saying that the DrillId property in the DrillCategory model is a foreign key that references the Id property in the Drill model.
+            // This is a one-to-many relationship between the Drill and DrillCategory models.
+            modelBuilder.Entity<DrillCategory>()
+                .HasOne(dc => dc.Drill)
+                .WithMany(d => d.DrillCategories)
+                .HasForeignKey(dc => dc.DrillId);
+
+            // Below we are defining the relationship between the Category and DrillCategory models.
+            // We are saying that a Category can have many DrillCategories.
+            // We are also saying that the CategoryId property in the DrillCategory model is a foreign key that references the Id property in the Category model.
+            modelBuilder.Entity<DrillCategory>()
+            .HasOne(dc => dc.Category)
+            .WithMany(c => c.DrillCategories)
+            .HasForeignKey(dc => dc.CategoryId);
+
+            // Below we are defining the relationship between the Drill and DrillEquipment models.
+            // We are saying that a Drill can have many DrillEquipments.
+            modelBuilder.Entity<DrillEquipment>()
+                .HasOne(de => de.Drill)
+                .WithMany(d => d.DrillEquipments)
+                .HasForeignKey(de => de.DrillId);
+
+            // Below we are defining the relationship between the Equipment and DrillEquipment models.
+            // We are saying that an Equipment can have many DrillEquipments.
+            modelBuilder.Entity<DrillEquipment>()
+                .HasOne(de => de.Equipment)
+                .WithMany(e => e.DrillEquipments)
+                .HasForeignKey(de => de.EquipmentId);
+        }
+
+    }
+}
---
+++ TrainingZoneDrills.Server/Migrations/20240702111522_InitialCreate.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrainingZoneDrills.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Drills",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Drills", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Equipments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Equipments", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DrillCategories",
+                columns: table => new
+                {
+                    DrillId = table.Column<int>(type: "int", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DrillCategories", x => new { x.DrillId, x.CategoryId });
+                    table.ForeignKey(
+                        name: "FK_DrillCategories_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DrillCategories_Drills_DrillId",
+                        column: x => x.DrillId,
+                        principalTable: "Drills",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DrillEquipments",
+                columns: table => new
+                {
+                    DrillId = table.Column<int>(type: "int", nullable: false),
+                    EquipmentId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DrillEquipments", x => new { x.DrillId, x.EquipmentId });
+                    table.ForeignKey(
+                        name: "FK_DrillEquipments_Drills_DrillId",
+                        column: x => x.DrillId,
+                        principalTable: "Drills",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DrillEquipments_Equipments_EquipmentId",
+                        column: x => x.EquipmentId,
+                        principalTable: "Equipments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DrillCategories_CategoryId",
+                table: "DrillCategories",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DrillEquipments_EquipmentId",
+                table: "DrillEquipments",
+                column: "EquipmentId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DrillCategories");
+
+            migrationBuilder.DropTable(
+                name: "DrillEquipments");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Drills");
+
+            migrationBuilder.DropTable(
+                name: "Equipments");
+        }
+    }
+}
